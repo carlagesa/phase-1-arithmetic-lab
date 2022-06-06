9faa1bb2709c7f14ca79ca8584c931b1bd076af8
carlton <carltonmbaj0@gmail.com>
2022-06-06 22:34:46 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 1000) + 1;
+
+// Modulus expression
+const num3 = 44;
+const num4 = 10;
+
+const mod = num3 % num4;
+
+//Max Value
+const max = Math.max(3, 4, 6, 11, 16, 19, 20);
